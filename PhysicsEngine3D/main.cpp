8ff661d3c2945e0eda6bc5f813ec27cb98ab6867
dPerez015 <davidperezgallego@enti.cat>
2019-03-17 03:48:21 +0100

    Added opengl libraries and init code to test them

---
+++ PhysicsEngine3D/main.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+#include <GL/glew.h>
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main() {
+	std::cout << "Hola\n";
+	if (!glfwInit()) {
+		fprintf(stderr, "Couldn't initialize GLFW\n");
+		return -1;
+	}
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		std::cout << "Error:	" << glewGetErrorString(err)<<std::endl;
+	}
+	std::cout << "GLEW: Version:	" << glewGetString(GLEW_VERSION);
+	int i = 0;
+}
